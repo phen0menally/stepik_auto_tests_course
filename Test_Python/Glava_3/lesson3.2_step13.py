0c955418db3b65b9e86436bdf550dff72bc24cd0
phen0menally <tim.abrikos@yandex.ru>
2022-06-03 19:29:45 +0300

    took another code to study

--- Test_Python/Glava_3/lesson3.2_step13.py
+++ Test_Python/Glava_3/lesson3.2_step13.py
@@ -1,53 +1,29 @@
-import unittest
 from selenium import webdriver
+import time
+import unittest
 
 
-class TestAbs(unittest.TestCase):
+def link_t(link):
+    browser = webdriver.Chrome()
+    browser.get(link)
 
-    def setUp(self):
-        self.browser = webdriver.Chrome()
+    browser.find_element_by_xpath(".//*[@placeholder = 'Введите имя']").send_keys("Иван")
+    browser.find_element_by_xpath(".//*[@placeholder = 'Введите фамилию']").send_keys("Иванов")
+    browser.find_element_by_xpath(".//*[@placeholder = 'Введите Email']").send_keys("ivan@yandex.ru")
+    browser.find_element_by_css_selector("button.btn").click()
 
-    def test_abs1(self):
-        browser = webdriver.Chrome()
-        browser.get("http://suninjuly.github.io/registration1.html")
-        input1 = browser.find_element_by_css_selector('[class="first_block"] > [class="form-group first_class"] > [class="form-control first"]')
-        input1.send_keys("Ivan")
-        input2 = browser.find_element_by_css_selector('[class="first_block"] > [class="form-group second_class"] > [class="form-control second"]')
-        input2.send_keys("Ivanovich")
-        input3 = browser.find_element_by_css_selector('[class="first_block"] > [class="form-group third_class"] > [class="form-control third"]')
-        input3.send_keys("proverka@yandex.ru")
+    time.sleep(1)
+    return browser.find_element_by_tag_name("h1").text
 
-        # Отправляем заполненную форму
-        button = browser.find_element_by_css_selector("button.btn")
-        button.click()
-        # находим элемент, содержащий текст
-        welcome_text_elt = browser.find_element_by_tag_name("h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
-        welcome_text = welcome_text_elt.text
 
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Should be equal Test1")
+class TestReg(unittest.TestCase):
+    def test_reg1(self):
+        self.assertEqual(link_t("http://suninjuly.github.io/registration1.html"),
+                         "Поздравляем! Вы успешно зарегистировались!", "registration is failed")
 
-    def test_abs2(self):
-        browser = webdriver.Chrome()
-        browser.get("http://suninjuly.github.io/registration2.html")
-        input1 = browser.find_element_by_css_selector('[class="first_block"] > [class="form-group first_class"] > [class="form-control first"]')
-        input1.send_keys("Ivan")
-        input2 = browser.find_element_by_css_selector('[class="first_block"] > [class="form-group second_class"] > [class="form-control second"]')
-        input2.send_keys("Ivanovich")
-        input3 = browser.find_element_by_css_selector('[class="first_block"] > [class="form-group third_class"] > [class="form-control third"]')
-        input3.send_keys("proverka@yandex.ru")
-
-        # Отправляем заполненную форму
-        button = browser.find_element_by_css_selector("button.btn")
-        button.click()
-        # находим элемент, содержащий текст
-        welcome_text_elt = browser.find_element_by_tag_name("h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
-        welcome_text = welcome_text_elt.text
-
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Should be equal Test2")
+    def test_reg2(self):
+        self.assertEqual(link_t("http://suninjuly.github.io/registration2.html"),
+                         "Поздравляем! Вы успешно зарегистировались!", "registration is failed")
 
 
 if __name__ == "__main__":
