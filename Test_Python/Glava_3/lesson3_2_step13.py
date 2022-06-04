e187eb979d56beeca3dbeb1556c6b3b20642f1e3
phen0menally <tim.abrikos@yandex.ru>
2022-06-04 19:30:25 +0300

    Test pytest information

--- Test_Python/Glava_3/lesson3_2_step13.py
+++ Test_Python/Glava_3/lesson3_2_step13.py
@@ -1,32 +1,30 @@
 from selenium import webdriver
-import time
-import unittest
+from selenium.webdriver.common.by import By
 
 
-def link_t(link):
-    browser = webdriver.Chrome()
-    browser.get(link)
-
-    browser.find_element_by_xpath(".//*[@placeholder = 'Введите имя']").send_keys("Иван")
-    browser.find_element_by_xpath(".//*[@placeholder = 'Введите фамилию']").send_keys("Иванов")
-    browser.find_element_by_xpath(".//*[@placeholder = 'Введите Email']").send_keys("ivan@yandex.ru")
-    browser.find_element_by_css_selector("button.btn").click()
-
-    time.sleep(1)
-    return browser.find_element_by_tag_name("h1").text
-
-
-class TestReg(unittest.TestCase):
-    def test_reg1(self):
+def registration(link):
+    try:
         browser = webdriver.Chrome()
         browser.get(link)
-        self.assertEqual(link_t("http://suninjuly.github.io/registration1.html"), "Поздравляем! Вы успешно зарегистировались!", "registration is failed")
+        browser.implicitly_wait(1)
+        first_name = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+        first_name.send_keys("Homer")
+        last_name = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+        last_name.send_keys("Simpson")
+        email = browser.find_element(By.CSS_SELECTOR, ".third_class .third")
+        email.send_keys("simpsons@mail.com")
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        return welcome_text
+    except:
+        browser.quit()
 
-    def test_reg2(self):
-        browser = webdriver.Chrome()
-        browser.get(link)
-        self.assertEqual(link_t("http://suninjuly.github.io/registration2.html"), "Поздравляем! Вы успешно зарегистировались!", "registration is failed")
+def test_link1():
+    link = "http://suninjuly.github.io/registration1.html"
+    assert registration(link) == "Congratulations! You have successfully registered!", "Error"
 
-
-if __name__ == "__main__":
-    unittest.main()
+def test_link2():
+    link = "http://suninjuly.github.io/registration2.html"
+    assert registration(link) == "Congratulations! You have successfully registered!", "Error"
